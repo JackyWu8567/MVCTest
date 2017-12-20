2768f15607e24415bfb0ff9fb5ae4d13d2027006
Jacky Wu (CSI Interfusion Inc) <v-wun@microsoft.com>
2017-12-20 16:34:59 +0800

    added a AuthResponse class in MVCTest.WebApi.Client project

---
+++ MVCTest/MVCTest/MVCTest.WebApi.Client/AuthResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCTest.WebApi.Client
+{
+    public class AuthResponse
+    {
+        public bool Authenticated { get; private set; }
+
+        public List<string> Roles { get; private set; }
+
+        public BearerToken Token { get; private set; }
+    }
+
+    public class BearerToken
+    {
+        public string AccessToken { get; private set; }
+
+        public DateTime Issued { get; private set; }
+
+        public DateTime Expired { get; private set; }
+    }
+}
